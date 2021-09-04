b0d1e49e5889cf7bb4a0f73f31499dfafd43c3f0
sasha stepakov <sanechkastepakov@gmail.com>
2021-09-04 17:38:30 +0300

    first commit

---
+++ database/factories/ArticleFactory.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Article;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class ArticleFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Article::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $title = $this->faker->sentence( 5, true );
+        $slug = Str::substr( Str::lower(  preg_replace( '/\s+/', '-', $title ) ), 0, -1 );
+
+        return [
+            'title' => $title,
+            'slug' => $slug,
+            'body' => $this->faker->paragraph( 100, false ),
+            'img' => 'https://via.placeholder/600/333/fff/?text=' . $title,
+            'published_at' => Carbon::now()
+        ];
+    }
+}
